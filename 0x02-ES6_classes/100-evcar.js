45b825f62fee5d494fea95db56e2330cf46c8569
Biruk Ephrem <birukeph@gmail.com>
2022-12-23 20:15:31 +0300

    es6 classes   - fixed lint   - removed a period from task 6

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,20 @@
-import Car from "./10-car";
+import Car from './10-car';
 
 export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-	get range() {
-		return this._range;
-	}
+  get range() {
+    return this._range;
+  }
 
-	set range(range) {
-		this._range = range;
-	}
+  set range(range) {
+    this._range = range;
+  }
 
-	cloneCar() {
+  cloneCar() {
     const Species = super.constructor[Symbol.species];
     return new Species();
   }
