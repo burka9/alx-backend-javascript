ec9fb151d35e83adb48d25e36b52434e49b45800
Biruk Ephrem <birukeph@gmail.com>
2022-12-23 21:58:27 +0300

    es6 promise   - fixed errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let user = null;
-  let photo = null;
+  let user;
+  let photo;
 
   try {
     user = await createUser();
     photo = await uploadPhoto();
   } catch (err) {
+    user = null;
+    photo = null;
   }
 
   return { photo, user };
